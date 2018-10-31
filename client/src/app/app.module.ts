19787045dfbb1f123b946ef99dd2682e8b138c17
Gianni Narracci <gnarracci@gmail.com>
2018-10-31 18:38:24 -0400

    Creación de Componentes y actualización de Tyscript a 3.1.1

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { OffersComponent } from './components/offers/offers.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { DetailsBookComponent } from './components/details-book/details-book.component';
+import { ListBooksComponent } from './components/admin/list-books/list-books.component';
+import { LoginComponent } from './components/user/login/login.component';
+import { RegisterComponent } from './components/user/register/register.component';
+import { ProfileComponent } from './components/user/profile/profile.component';
+import { Page404Component } from './components/page404/page404.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    OffersComponent,
+    HeroComponent,
+    DetailsBookComponent,
+    ListBooksComponent,
+    LoginComponent,
+    RegisterComponent,
+    ProfileComponent,
+    Page404Component
   ],
   imports: [
     BrowserModule,
